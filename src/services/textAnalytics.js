b7ad1ece66ce4878a0fc0ebd6615ed04f3f944c4
Yasmin Teles <teles.yasmin@gmail.com>
2020-09-02 22:55:09 -0300

    feat: add text analytics services

---
+++ src/services/textAnalytics.js
@@ -0,0 +1,43 @@
+import { TextAnalyticsClient, AzureKeyCredential } from "@azure/ai-text-analytics"
+
+// const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics")
+
+const key = "83200d7334d4496180aaca4d6ffa9ddb"
+const endpoint = "https://clinicall.cognitiveservices.azure.com/"
+
+const textAnalyticsClient = new TextAnalyticsClient(endpoint, new AzureKeyCredential(key))
+
+async function sentimentAnalysis(client) {
+  const sentimentInput = [
+    "I had the best day of my life. I wish you were there with me.",
+  ]
+  const sentimentResult = await client.analyzeSentiment(sentimentInput)
+
+  sentimentResult.forEach((document) => {
+    console.log(`ID: ${document.id}`)
+    console.log(`\tDocument Sentiment: ${document.sentiment}`)
+    console.log("\tDocument Scores:")
+    console.log(`\t\tPositive: ${document.confidenceScores.positive.toFixed(2)} \tNegative: ${document.confidenceScores.negative.toFixed(2)} \tNeutral: ${document.confidenceScores.neutral.toFixed(2)}`)
+    console.log(`\tSentences Sentiment(${document.sentences.length}):`)
+    document.sentences.forEach((sentence) => {
+      console.log(`\t\tSentence sentiment: ${sentence.sentiment}`)
+      console.log("\t\tSentences Scores:")
+      console.log(`\t\tPositive: ${sentence.confidenceScores.positive.toFixed(2)} \tNegative: ${sentence.confidenceScores.negative.toFixed(2)} \tNeutral: ${sentence.confidenceScores.neutral.toFixed(2)}`)
+    })
+  })
+}
+
+sentimentAnalysis(textAnalyticsClient)
+
+async function keyPhraseExtraction(client) {
+  const keyPhrasesInput = [
+    "My cat might need to see a veterinarian.",
+  ]
+  const keyPhraseResult = await client.extractKeyPhrases(keyPhrasesInput)
+
+  keyPhraseResult.forEach((document) => {
+    console.log(`ID: ${document.id}`)
+    console.log(`\tDocument Key Phrases: ${document.keyPhrases}`)
+  })
+}
+keyPhraseExtraction(textAnalyticsClient)
